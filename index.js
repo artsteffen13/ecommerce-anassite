9d512cf5aa99604d0dfe2082d6eba0f8c398fb47
artsteffen13 <artsteffen13@hotmail.com>
2020-09-09 12:56:52 -0400

    attempted to correct cookie expiration time

--- index.js
+++ index.js
@@ -14,6 +14,7 @@ const app = express();
 const sess = {
     secret: keys.secret,
     cookie: {maxAge: 60 * 60 * 24},
+    maxAge: 60*60*24,
     resave: false,
     saveUninitialized: false,
     store: new MongoStore({ mongooseConnection: mongoose.connection })
